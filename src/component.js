1ef22766bc4e62430e2b25ca26d93fc09b0a4a06
linjieChen <chenlinjiefind@163.com>
2017-05-07 12:57:14 +0800

    事件

--- src/component.js
+++ src/component.js
@@ -9,15 +9,42 @@ class MyComponent extends Component {
     //         defaultProps:"初始值"
     //     }
     // }
-    constructor(){
+    constructor(){ 
+        // 定义初始值
         //添加super相当于将React.Componet new出来
         super();
         this.state={
-            textVaule:'这是初始值'
+            textVaule:'这是初始值',
+            number:1
         }
     }
+    handleAdd(e){
+        console.log(e);
+        console.log(this);
+        var numbers = this.state.number + 1;
+        this.setState({
+            number: numbers
+        });
+    }
+    // 已插入真实DOM之前
     componentWillMount(){
+        console.log(this.state.textVaule);  // 这是初始值 打印之前的数据
+        console.log("已插入真实DOM之前");
+        // setState是用于设置state中的参数
         this.setState({textVaule:'通过setState修改的值'});
+        console.log(this.state.textVaule);  // 这是初始值 打印之前的数据
+    }
+    // 已插入真实DOM之后
+    componentDidMount(){
+        console.log("已插入真实DOM之后");
+    }
+    // 正在被重新渲染之前
+    componentWillUpdate(){
+        console.log('//将要更新');
+    }
+    // 正在被重新渲染之后
+    componentDidUpdate(){
+        console.log('//已经更新');
     }
     render() {
         return (
@@ -34,6 +61,8 @@ class MyComponent extends Component {
                 {
                     // <h1>{this.props.defaultProps}</h1>
                 }
+                <input type="button" value="点击" onClick={this.handleAdd.bind(this)} />
+                <p>{this.state.number}</p>
                 <h1>{this.state.textVaule}</h1>
                 <h1> Hello world!!! </h1>  <h2> {this.props.title},{this.props.myCom}</h2> 
             </div>
